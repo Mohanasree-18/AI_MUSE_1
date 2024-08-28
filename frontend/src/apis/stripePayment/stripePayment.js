e694a8ac62a19a5dd70079aba209afd421c719dc
Mohanasree-18 <ugs21023_aids.sree@cbit.org.in>
2024-08-28 14:38:06 +0530

    Update stripePayment.js with backend url

--- frontend/src/apis/stripePayment/stripePayment.js
+++ frontend/src/apis/stripePayment/stripePayment.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //stripe payment
 export const handleFreeSubscriptionAPI = async () => {
   const response = await axios.post(
-    "https://ai-muse-1-mohanas-projects-c3f3df45.vercel.app/free-plan",
+    "https://ai-muse-1.vercel.app/free-plan",
     {},
     {
       withCredentials: true,
