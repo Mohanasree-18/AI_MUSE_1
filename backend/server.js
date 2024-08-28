e9a59acaa2ef6fc65844151c2872508cf5608a8e
Mohanasree-18 <ugs21023_aids.sree@cbit.org.in>
2024-08-28 10:49:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -104,7 +104,7 @@ app.use(cookieParser()); //to parse cookies
 
 //!cors
 const corsOptions = {
-  origin: "https://ai-muse-1-frontend-lfp1itn5l-mohanas-projects-c3f3df45.vercel.app",
+  origin: "https://ai-muse-1-frontend.vercel.app",
   credentials: true, //this arg is for cookies
 };
 app.use(cors(corsOptions));
